b146a218f470591aa686db4552f31d3200f1cc9d
Jet (Ta-Wei Chien)$windows <67193495+jet-c-21@users.noreply.github.com>
2023-02-04 13:45:09 +0800

    Update 0239. Sliding Window Maximum.py

--- Hard/0239. Sliding Window Maximum/0239. Sliding Window Maximum.py
+++ Hard/0239. Sliding Window Maximum/0239. Sliding Window Maximum.py
@@ -4,23 +4,30 @@ import collections
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        output = []
+        res = []
         q = collections.deque()  # index
-        l = r = 0
+        l_idx = r_idx = 0
         # O(n) O(n)
-        while r < len(nums):
+        while r_idx < len(nums):
             # pop smaller values from q
-            while q and nums[q[-1]] < nums[r]:
+            while q and nums[q[-1]] < nums[r_idx]:
                 q.pop()
-            q.append(r)
+            q.append(r_idx)
 
             # remove left val from a window
-            if l > q[0]:
+            if l_idx > q[0]:
                 q.popleft()
 
-            if (r + 1) >= k:
-                output.append(nums[q[0]])
-                l += 1
-            r += 1
+            if (r_idx + 1) >= k:
+                res.append(nums[q[0]])
+                l_idx += 1
 
-        return output
+            r_idx += 1
+
+        return res
+
+
+if __name__ == '__main__':
+    nnn = [1, 3, -1, -3, 5, 3, 6, 7]
+    kkk = 3
+    print(Solution().maxSlidingWindow(nnn, kkk))
